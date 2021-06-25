1089b2648905eb00ce85e9c434cfc0270f2aa6c7
zhanglinbo <nevermiss21@126.com>
2021-06-25 19:20:17 +0800

    V0.1.15版本，调整输出目录结构，所有输出文件统一在Output，对应的配置下会直接找到.dll和exe

--- HmiPart/HmiComm/src/VersionMgr.cpp
+++ HmiPart/HmiComm/src/VersionMgr.cpp
@@ -1,7 +1,7 @@
 #include "VersionMgr.h"
 #include <string>
 //Version hmiVer = {0,1,1}; //HMI程序版本
-HMIVersion hmiVer = { 0,1,14 }; //HMI程序版本
+HMIVersion hmiVer = { 0,1,15 }; //HMI程序版本
 std::string GetVersionInfo()
 {
 	std::string versionNo = 'V' + std::to_string(hmiVer.Main) + '.' + std::to_string(hmiVer.Sub) + '.' + std::to_string(hmiVer.Order)+'.'+std::string(__DATE__);
