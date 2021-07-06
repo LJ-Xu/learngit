cefd8cf02a40ed839205038dbd0105e254adc970
zhanglinbo <nevermiss21@126.com>
2021-07-06 11:26:00 +0800

    解决消息队列，get后释放问题

--- HmiPart/MsgQueue/src/ListQueue.cpp
+++ HmiPart/MsgQueue/src/ListQueue.cpp
@@ -45,11 +45,10 @@ namespace Message
 	{
 		ListQueueItem* tmp;//mtx_
 		ListQueueItem* item;
-		mtx_.lock();
+		std::lock_guard<std::mutex> lck(mtx_);
 		item = NewItem();
 		if (!item)
 		{
-			mtx_.unlock();
 			return false;
 		}
 		item->Msg.Hwnd = hwnd;
@@ -61,14 +60,20 @@ namespace Message
 
 		tail_->Next = item;
 		tail_ = item;
-		mtx_.unlock();
 		return true;
 
 	}
+	/*bool ListQueue::FreeMsg(Msg* msg)
+	{
+		std::lock_guard<std::mutex> lck(mtx_);
+		ListQueueItem * lqt = free_.Next;
+		((ListQueueItem *)msg)->Next = lqt;
+		free_.Next =(ListQueueItem *) msg;
+	}*/
 	bool ListQueue::GetMsg(Msg& msg)
 	{
 		ListQueueItem* tmp;
-		mtx_.lock();
+		std::lock_guard<std::mutex> lck(mtx_);
 		if (head_.Next)
 		{
 			tmp = head_.Next;
@@ -76,10 +81,11 @@ namespace Message
 			if (!head_.Next)
 				tail_ = &head_;
 			msg = tmp->Msg;
-			mtx_.unlock();
+			//放入空闲列表
+			tmp->Next = free_.Next;
+			free_.Next = tmp;
 			return true;
 		}
-		mtx_.unlock();
 		return false;
 	}
 	ListQueue::~ListQueue()
